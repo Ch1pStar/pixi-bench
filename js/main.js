06ac2b95dd4f30d662c44cf61bba7418f4f69c61
Nedelin Yordanov <nedeliny@gmail.com>
2017-02-23 18:20:54 +0200

    Add .fla tests

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 'use strict'
 
 let renderer;
+const Anim = PIXI.animate;
 
 const init = ()=>{
   renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
   // renderer = new PIXI.CanvasRenderer(window.innerWidth, window.innerHeight);
+  renderer.backgroundColor = 0xcccccc;
+
   document.body.appendChild(renderer.view);
 
   const stage = new PIXI.Container();
+
+  const currMc = lib.ColorTween;
+
+  Anim.load(currMc, (instance) => {
+    window.instance = instance;
+    console.log(instance);
+    // const c1 = instance.children[0];
+    // stage.addChild(c1);
+
+    instance.framerate = 5;
+
+    stage.addChild(instance);
+  });
 
   const draw = t=>{
     requestAnimationFrame(draw);
